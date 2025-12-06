b2bf27a31fa092db478e9201d3ddb931858c93d1
seowfie <sarmiento21sofia@gmail.com>
2025-12-06 18:18:23 +0800

    fix app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Employee } from './employee/employee';
+import { Services } from './services/services';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Home},
+  { path: 'about', component: About},
+  { path: 'employee', component: Employee },
+  { path: 'services', component: Services }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
